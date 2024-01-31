198c5bdab492f6c5deb42ff14b0ad69c4d036970
Vitor Vilete <vitorgarcesvilete@gmail.com>
2024-01-31 17:28:32 -0300

    First draft of a multi-module package structure; has problems with dependency injection

--- backendchallenge-domain/src/main/java/br/com/vilevidya/backendchallenge/domain/entity/InsuranceTypes/InsuranceType.java
+++ backendchallenge-domain/src/main/java/br/com/vilevidya/backendchallenge/domain/entity/InsuranceTypes/InsuranceType.java
@@ -1,7 +1,4 @@
 package br.com.vilevidya.backendchallenge.domain.entity.InsuranceTypes;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 public record InsuranceType(String name, Number iofTaxValue, Number pisTaxValue, Number cofinsTaxValue) {
 }
---
+++ backendchallenge-presentation/src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS INSURANCE_PRODUCT (
+    ID UUID not null,
+    NAME varchar(255) not null,
+    CATEGORY varchar(255) not null,
+    BASE_PRICE decimal(11,2) not null,
+    TAXED_PRICE decimal(11,2) not null
+);
+
+//ALTER TABLE INSURANCE_PRODUCT
+//ADD CONSTRAINT PK_InsuranceProduct
+//PRIMARY KEY (ID, NAME, CATEGORY);
+
+CREATE TABLE IF NOT EXISTS INSURANCE_TYPE (
+    NAME varchar(255) primary key,
+    IOF_TAX_VALUE decimal(11, 5),
+    PIS_TAX_VALUE decimal(11, 5),
+    COFINS_TAX_VALUE decimal(11, 5)
+);
