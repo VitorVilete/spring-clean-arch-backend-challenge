c5e61a85a969a90026914b2754fb906531a81236
Vitor Vilete <vitorgarcesvilete@gmail.com>
2024-02-02 18:58:08 -0300

    refactoring application unit tests

--- backendchallenge-application/src/test/java/br/com/vilevidya/backendchallenge/application/usecases/InsuranceProducts/CreateInsuranceProductUseCaseTest.java
+++ backendchallenge-application/src/test/java/br/com/vilevidya/backendchallenge/application/usecases/InsuranceProducts/CreateInsuranceProductUseCaseTest.java
@@ -4,6 +4,7 @@ import br.com.vilevidya.backendchallenge.application.interfaces.InsuranceProduct
 import br.com.vilevidya.backendchallenge.domain.entity.InsuranceProducts.InsuranceProduct;
 import br.com.vilevidya.backendchallenge.domain.entity.InsuranceTypes.InsuranceType;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,69 +29,15 @@ class CreateInsuranceProductUseCaseTest {
     @InjectMocks
     CreateInsuranceProductUseCase createInsuranceProductUseCase;
 
-    public static final String NAME = "test";
-    public static final UUID ID = UUID.randomUUID();
-    public static final double BASE_PRICE_DOUBLE = BigDecimal.valueOf(100.00).doubleValue();
-    public static final double TAXED_PRICE_DOUBLE = BigDecimal.valueOf(101.00).doubleValue();
+    String insuranceProductName;
+    UUID insuranceProductId;
+    double insuranceProductBasePrice;
+    double insuranceProductTaxedPrice;
 
-    private InsuranceProduct buildInsuranceProduct(){
-        return new InsuranceProduct.InsuranceProductBuilder()
-                .setInsuranceType(new InsuranceType.InsuranceTypeBuilder()
-                        .setCofinsTaxValue(0)
-                        .setIofTaxValue(0)
-                        .setPisTaxValue(0)
-                        .build())
-                .setId(ID)
-                .setName(NAME)
-                .setBasePrice(BASE_PRICE_DOUBLE)
-                .setTaxedPrice(TAXED_PRICE_DOUBLE)
-                .build();
-    }
-    private InsuranceProduct buildInsuranceWithoutTax(){
-        return new InsuranceProduct.InsuranceProductBuilder()
-                .setInsuranceType(new InsuranceType.InsuranceTypeBuilder()
-                        .setCofinsTaxValue(0)
-                        .setIofTaxValue(0)
-                        .setPisTaxValue(0)
-                        .build())
-                .setId(ID)
-                .setName(NAME)
-                .setBasePrice(BASE_PRICE_DOUBLE)
-                .setTaxedPrice(0)
-                .build();
-    }
+    InsuranceType insuranceType;
+    InsuranceProduct insuranceProduct;
+    InsuranceProduct insuranceProductWithoutTaxes;
 
-    @Test
-    public void CreateInsuranceProductUseCaseTest_createInsuranceProduct_shouldCallIInsuranceProductGatewayOnce(){
-        //Arrange
-        when(IInsuranceProductGateway.createInsuranceProduct(Mockito.any(InsuranceProduct.class)))
-                .thenReturn(buildInsuranceProduct());
-
-        //Act
-        InsuranceProduct result = createInsuranceProductUseCase.createInsuranceProduct(buildInsuranceProduct());
-
-        //Assert
-        verify(IInsuranceProductGateway, times(1)).createInsuranceProduct(Mockito.any(InsuranceProduct.class));
-    }
-
-    @Test
-    public void CreateInsuranceProductUseCaseTest_calculateTaxes_shouldCalculateTaxesCorrectly(){
-        //Arrange
-        InsuranceProduct insuranceProductAlreadyTaxed = buildInsuranceProduct();
-        InsuranceProduct insuranceProductWithoutTax = buildInsuranceWithoutTax();
-
-        //Act
-        insuranceProductAlreadyTaxed.setTaxedPrice(createInsuranceProductUseCase.calculateTaxes(insuranceProductAlreadyTaxed));
-        double calculatedTaxesAlreadyTaxed = calculateTaxes(insuranceProductAlreadyTaxed);
-        insuranceProductWithoutTax.setTaxedPrice(createInsuranceProductUseCase.calculateTaxes(insuranceProductWithoutTax));
-        double calculatedTaxesNoTaxes = calculateTaxes(insuranceProductWithoutTax);
-        //Assert
-        Assertions.assertThat(insuranceProductWithoutTax.getTaxedPrice()).isEqualTo(calculatedTaxesNoTaxes);
-        Assertions.assertThat(insuranceProductAlreadyTaxed.getTaxedPrice()).isEqualTo(calculatedTaxesAlreadyTaxed);
-
-
-
-    }
     private double calculateTaxes(InsuranceProduct insuranceProduct){
         double result;
         if (insuranceProduct.getTaxedPrice() == 0){
@@ -105,6 +52,65 @@ class CreateInsuranceProductUseCaseTest {
             result = insuranceProduct.getTaxedPrice();
         }
         return result;
+    }
+
+    @BeforeEach
+    public void init(){
+        insuranceProductName = "test";
+        insuranceProductId = UUID.randomUUID();
+        insuranceProductBasePrice = BigDecimal.valueOf(100.00).doubleValue();
+        insuranceProductTaxedPrice = BigDecimal.valueOf(101.00).doubleValue();
+        insuranceType = new InsuranceType.InsuranceTypeBuilder()
+                .setCofinsTaxValue(0)
+                .setIofTaxValue(0)
+                .setPisTaxValue(0)
+                .build();
+        insuranceProduct = new InsuranceProduct.InsuranceProductBuilder()
+                .setInsuranceType(insuranceType)
+                .setId(insuranceProductId)
+                .setName(insuranceProductName)
+                .setBasePrice(insuranceProductBasePrice)
+                .setTaxedPrice(insuranceProductTaxedPrice)
+                .build();
+        insuranceProductWithoutTaxes = new InsuranceProduct.InsuranceProductBuilder()
+                .setInsuranceType(insuranceType)
+                .setId(insuranceProductId)
+                .setName(insuranceProductName)
+                .setBasePrice(insuranceProductBasePrice)
+                .setTaxedPrice(0)
+                .build();
+    }
+
+    @Test
+    public void CreateInsuranceProductUseCaseTest_createInsuranceProduct_shouldCallIInsuranceProductGatewayOnce(){
+        //Arrange
+        when(IInsuranceProductGateway.createInsuranceProduct(Mockito.any(InsuranceProduct.class)))
+                .thenReturn(insuranceProduct);
+
+        //Act
+        InsuranceProduct result = createInsuranceProductUseCase.createInsuranceProduct(insuranceProduct);
+
+        //Assert
+        verify(IInsuranceProductGateway, times(1)).createInsuranceProduct(Mockito.any(InsuranceProduct.class));
+    }
+
+    @Test
+    public void CreateInsuranceProductUseCaseTest_calculateTaxes_shouldCalculateTaxesCorrectly(){
+        //Arrange
+        InsuranceProduct insuranceProductAlreadyTaxed = insuranceProduct;
+        InsuranceProduct insuranceProductWithoutTax = insuranceProductWithoutTaxes;
+
+        //Act
+        insuranceProductAlreadyTaxed.setTaxedPrice(createInsuranceProductUseCase.calculateTaxes(insuranceProductAlreadyTaxed));
+        double calculatedTaxesAlreadyTaxed = calculateTaxes(insuranceProductAlreadyTaxed);
+        insuranceProductWithoutTax.setTaxedPrice(createInsuranceProductUseCase.calculateTaxes(insuranceProductWithoutTax));
+        double calculatedTaxesNoTaxes = calculateTaxes(insuranceProductWithoutTax);
+        //Assert
+        Assertions.assertThat(insuranceProductWithoutTax.getTaxedPrice()).isEqualTo(calculatedTaxesNoTaxes);
+        Assertions.assertThat(insuranceProductAlreadyTaxed.getTaxedPrice()).isEqualTo(calculatedTaxesAlreadyTaxed);
+
+
+
     }
 
 }
--- backendchallenge-application/src/test/java/br/com/vilevidya/backendchallenge/application/usecases/InsuranceTypes/FindInsuranceTypeByNameUseCaseTest.java
+++ backendchallenge-application/src/test/java/br/com/vilevidya/backendchallenge/application/usecases/InsuranceTypes/FindInsuranceTypeByNameUseCaseTest.java
@@ -4,6 +4,7 @@ import br.com.vilevidya.backendchallenge.application.interfaces.InsuranceTypes.I
 import br.com.vilevidya.backendchallenge.application.usecases.exceptions.InsuranceTypeNotFoundException;
 import br.com.vilevidya.backendchallenge.domain.entity.InsuranceTypes.InsuranceType;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,23 +12,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FindInsuranceTypeByNameUseCaseTest {
-
-    public static final String VIDA = "VIDA";
-    public static final BigDecimal IOF_TAX_VALUE = BigDecimal.valueOf(0.01);
-    public static final BigDecimal PIS_TAX_VALUE = BigDecimal.valueOf(0.022);
-    public static final BigDecimal COFINS_TAX_VALUE = BigDecimal.valueOf(0);
-    public static final String CATEGORIA_NAO_ENCONTRADA_COM_O_NOME_VIDA = "Categoria não encontrada com o nome: VIDA";
 
     @Mock
     IInsuranceTypeGateway iInsuranceTypeGateway;
@@ -36,36 +28,47 @@ class FindInsuranceTypeByNameUseCaseTest {
     @InjectMocks
     FindInsuranceTypeByNameUseCase findInsuranceTypeByNameUseCase;
 
-    private InsuranceType createInsuranceType(){
-        return new InsuranceType.InsuranceTypeBuilder(VIDA, IOF_TAX_VALUE, PIS_TAX_VALUE, COFINS_TAX_VALUE).build();
+    private InsuranceType insuranceType;
+    private String insuranceTypeName;
+    private BigDecimal insuranceTypeIofTaxValue;
+    private BigDecimal insuranceTypePisTaxValue;
+    private BigDecimal insuranceTypeCofinsTaxValue;
+    private String insuranceTypeNotFoundExceptionMessage;
+
+    @BeforeEach
+    public void init(){
+        insuranceType = new InsuranceType.InsuranceTypeBuilder(insuranceTypeName, insuranceTypeIofTaxValue, insuranceTypePisTaxValue, insuranceTypeCofinsTaxValue).build();
+        insuranceTypeName = "VIDA";
+        insuranceTypeIofTaxValue = BigDecimal.valueOf(0.01);
+        insuranceTypePisTaxValue = BigDecimal.valueOf(0.022);
+        insuranceTypeCofinsTaxValue = BigDecimal.valueOf(0);
+        insuranceTypeNotFoundExceptionMessage = "Categoria não encontrada com o nome: VIDA";
     }
 
     @Test
     public void FindInsuranceTypeByNameUseCase_findInsuranceTypeByName_returnInsuranceType() throws InsuranceTypeNotFoundException {
         //Arrange
         when(iInsuranceTypeGateway.findInsuranceTypeByName(Mockito.any(String.class)))
-                .thenReturn(createInsuranceType());
+                .thenReturn(insuranceType);
         //Act
-        InsuranceType resultInsuranceType = findInsuranceTypeByNameUseCase.findInsuranceTypeByName(VIDA);
+        InsuranceType resultInsuranceType = findInsuranceTypeByNameUseCase.findInsuranceTypeByName(insuranceTypeName);
         //Assert
         Assertions.assertThat(resultInsuranceType).isNotNull();
-
-
     }
 
     @Test
     public void FindInsuranceTypeByNameUseCase_findInsuranceTypeByName_ThrowInsuranceTypeNotFoundException() throws InsuranceTypeNotFoundException {
         //Arrange
         when(iInsuranceTypeGateway.findInsuranceTypeByName(Mockito.any(String.class)))
-                .thenThrow(new InsuranceTypeNotFoundException(CATEGORIA_NAO_ENCONTRADA_COM_O_NOME_VIDA));
+                .thenThrow(new InsuranceTypeNotFoundException(insuranceTypeNotFoundExceptionMessage));
 
         //Act
         Throwable exception = catchThrowable(() -> {
-            InsuranceType resultInsuranceType = findInsuranceTypeByNameUseCase.findInsuranceTypeByName(VIDA);
+            findInsuranceTypeByNameUseCase.findInsuranceTypeByName(insuranceTypeName);
         });
 
         //Assert
-        Assertions.assertThat(exception).isInstanceOf(InsuranceTypeNotFoundException.class).hasMessage(CATEGORIA_NAO_ENCONTRADA_COM_O_NOME_VIDA);
+        Assertions.assertThat(exception).isInstanceOf(InsuranceTypeNotFoundException.class).hasMessage(insuranceTypeNotFoundExceptionMessage);
     }
 
 }
